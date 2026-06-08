084fece8508e07c92440a63127334b2a5ce33277
ClariceOliveira07 <claricerodrigues071321@gmail.com>
2026-06-08 11:50:14 -0300

    atualizacao

--- barbearia/routes/web.php
+++ barbearia/routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClientController;
+use App\Http\Controllers\ServiceController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::resource('clients', ClientController::class);
+Route::resource('services', ServiceController::class);
