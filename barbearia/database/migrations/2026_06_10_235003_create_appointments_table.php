03d7847d722550af2486634771f5270e39a5e1d9
ClariceOliveira07 <claricerodrigues071321@gmail.com>
2026-06-10 21:00:16 -0300

    crud 3 iniciado

---
+++ barbearia/database/migrations/2026_06_10_235003_create_appointments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('client_id')->constrained('clients')->onDelete('cascade');
+            $table->foreignId('service_id')->constrained('services')->onDelete('restrict');
+            $table->date('data_agenda');
+            $table->time('hora_agenda');
+            $table->enum('status', ['Pendente', 'Concluído', 'Cancelado'])->default('Pendente');
+            $table->text('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('appointments');
+    }
+};
